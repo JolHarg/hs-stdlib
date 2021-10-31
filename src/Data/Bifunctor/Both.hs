e5ece9198cbb5cff5f578d9ed5ee3e11ea523d3b
Dan Dart <git@dandart.co.uk>
2021-10-31 04:15:46 +0000

    lints

--- src/Data/Bifunctor/Both.hs
+++ src/Data/Bifunctor/Both.hs
@@ -4,5 +4,5 @@ module Data.Bifunctor.Both where
 import           Data.Bifunctor
 
 -- relude Relude.Extra.Bifunctor
-bimapBoth ∷ Bifunctor f ⇒ (a → b) → f a a -> f b b
+bimapBoth ∷ Bifunctor f ⇒ (a → b) → f a a → f b b
 bimapBoth f = bimap f f
