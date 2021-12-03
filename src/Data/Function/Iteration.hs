d559b2085d82ec7513c8020f36904a446322455f
Dan Dart <git@dandart.co.uk>
2021-12-03 17:32:47 +0000

    Fix Iteration and Conversion

--- src/Data/Function/Iteration.hs
+++ src/Data/Function/Iteration.hs
@@ -1,13 +1,15 @@
+{-# LANGUAGE UnicodeSyntax #-}
+
 module Data.Function.Iteration where
 
 -- similar to utility-ht's nest, or applyN, nTimes, etc
-iter ∷ Int → (a → a) → a -> a
+iter ∷ Int → (a → a) → a → a
 iter 0 _ x = x
 iter 1 f x = f x
 iter i f x = iter (i - 1) f x
 
 -- utility-ht's Control.Monad.HT's nest
-iterA ∷ Applicative f ⇒ Int → (a → f a) → a -> f a
+iterA ∷ Applicative f ⇒ Int → (a → f a) → a → f a
 iterA 0 _ x = pure x
 iterA 1 f x = f x
 iterA i f x = iterA (i - 1) f x
--- src/Data/Time/Conversion.hs
+++ src/Data/Time/Conversion.hs
@@ -5,5 +5,5 @@ import           Data.Time.Constants (secondsInYear)
 import           Number.SI.Unit (secondsPerDay)
 
 daysToSeconds, yearsToSeconds ∷ Double → Integer
-daysToSeconds d = round (d * fromInteger secondsPerDay)
+daysToSeconds d = round (d * secondsPerDay)
 yearsToSeconds d = round (d * fromInteger secondsInYear)
