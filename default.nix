fe40ed5d3e56c5ae49ed18f43968ba4c04024abe
Dan Dart <git@dandart.co.uk>
2022-03-24 01:45:04 +0000

    disable haddock, upgrades and disable webdriver for now

--- default.nix
+++ default.nix
@@ -1,18 +1,20 @@
 { 
-  nixpkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz") {},
-  compiler ? "ghc921"
+  nixpkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/haskell-updates.tar.gz") {},
+  haskell-tools ? import (builtins.fetchTarball "https://github.com/danwdart/haskell-tools/archive/master.tar.gz") {},
+  compiler ? "ghc922"
 } :
 let
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
+  tools = haskell-tools compiler;
   lib = nixpkgs.pkgs.haskell.lib;
   myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
-      hs-stdlib = self.callCabal2nix "hs-stdlib" (gitignore ./.) {};
+      hs-stdlib = lib.dontHaddock (self.callCabal2nix "hs-stdlib" (gitignore ./.) {});
       # https://github.com/kallisti-dev/hs-webdriver/issues/177
-      webdriver = self.callCabal2nix "webdriver" (builtins.fetchGit {
-        url = "https://github.com/danwdart/hs-webdriver.git";
-        rev = "52a82be322cbb8ee8e65f87056827a3b89277e2a";
-      }) {};
+      #webdriver = self.callCabal2nix "webdriver" (builtins.fetchGit {
+      #  url = "https://github.com/danwdart/hs-webdriver.git";
+      #  rev = "52a82be322cbb8ee8e65f87056827a3b89277e2a";
+      #}) {};
     };
   };
   shell = myHaskellPackages.shellFor {
@@ -23,20 +25,7 @@ let
       gen-hie > hie.yaml
       for i in $(find -type f); do krank $i; done
     '';
-    buildInputs = with myHaskellPackages; with nixpkgs; with haskellPackages; [
-      apply-refact
-      cabal-install
-      ghcid
-      ghcide
-      haskell-language-server
-      hasktags
-      hlint
-      implicit-hie
-      krank
-      stan
-      stylish-haskell
-      weeder
-    ];
+    buildInputs = tools.defaultBuildTools;
     withHoogle = false;
   };
   exe = lib.justStaticExecutables (myHaskellPackages.hs-stdlib);
